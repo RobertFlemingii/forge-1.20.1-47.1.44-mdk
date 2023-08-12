cb0c1f40590346ce90ce257732c0483fc61a891b
RobertFlemingii <gnimelftrebor@gmail.com>
2023-08-12 15:29:45 -0700

    Finished implementing Wolf Whistle Item

---
+++ src/main/java/net/nOObplus1/newfriends/item/ModItems.java
@@ -0,0 +1,20 @@
+package net.nOObplus1.newfriends.item;
+
+import net.nOObplus1.newfriends.NewFriends;
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS =
+            DeferredRegister.create(ForgeRegistries.ITEMS, NewFriends.MOD_ID);
+
+    public static final RegistryObject<Item> WOLFWHISTLE = ITEMS.register("wolfwhistle",
+            () -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus) {
+        ITEMS.register(eventBus);
+    }
+}
